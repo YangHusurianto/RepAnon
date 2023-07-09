8904e604aee805d40abd2b895b0012c5ff6d6ef9
Yang Husurianto <yang.husurianto@gmail.com>
2023-07-09 13:04:18 -0700

    Move events to separate file

---
+++ events/interactionCreate.js
@@ -0,0 +1,23 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		// check if command exists
+		if (!command) {
+			console.error(`${interaction.commandName} command not found!`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
+	}
+}
--- index.js
+++ index.js
@@ -8,11 +8,6 @@ require("dotenv").config();
 // Create client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When client ready, run following once
-client.once(Events.ClientReady, c => {
-	console.log(`Bot logged in as ${c.user.tag}`);
-});
-
 // register slash commands
 client.commands = new Collection();
 
@@ -30,30 +25,6 @@ for (const file of commandFiles) {
 		console.log(`[WARNING] ${filePath} command failed to register properly`);
 	}
 }
-
-// setup slash command responses
-client.on(Events.ItneractionCreate, interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	// check if command exists
-	if (!command) {
-		console.error(`${interaction.commandName} command not found!`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true });
-		}
-	}
-});
 
 // Log in with token
 client.login();
