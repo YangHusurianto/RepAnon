bff5d2bce23a8d2df52e21f5818e181863dcb98f
Yang Husurianto <yang.husurianto@gmail.com>
2023-07-12 16:26:37 -0700

    Add avatar changing on bot ready

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,8 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		// set bot avatar
+		client.user.setAvatar('./avatar.png')
 		console.log(`Bot logged in as ${client.user.tag}`);
 	}
 }
