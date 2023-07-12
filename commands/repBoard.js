9321342c338b2db22675d2d21369b0fc4b1498bd
Yang Husurianto <yang.husurianto@gmail.com>
2023-07-12 12:02:19 -0700

    Flip boolean for reversing board

--- commands/repBoard.js
+++ commands/repBoard.js
@@ -26,8 +26,8 @@ module.exports = function(repDB) {
 			}
 
 			const sortedUsers = (interaction.options.getBoolean("reverse") 
-								? users.sort((a, b) => (a.rep > b.rep) ? -1 : 1)
-								: users.sort((a, b) => (a.rep > b.rep) ? 1 : -1);
+								? users.sort((a, b) => (a.rep > b.rep) ? 1 : -1)
+								: users.sort((a, b) => (a.rep > b.rep) ? -1 : 1);
 
 			const repEmbed = new EmbedBuilder()
 				.setTitle(`Top 10 Rep in ${guild.name}`)
