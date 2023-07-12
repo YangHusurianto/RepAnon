4a08adffe8e72e1d5ea546dfecdaf8972cb65ada
Yang Husurianto <yang.husurianto@gmail.com>
2023-07-12 16:28:46 -0700

    Add await for avatar set promise

--- events/ready.js
+++ events/ready.js
@@ -6,6 +6,9 @@ module.exports = {
 	execute(client) {
 		// set bot avatar
 		client.user.setAvatar('./avatar.png')
+			.then(user => console.log(`New avatar set!`))
+			.catch(console.error);
+  
 		console.log(`Bot logged in as ${client.user.tag}`);
 	}
 }
