9c1bdcb2879375fdcc13e8828a1ca32dc601026f
Yang Husurianto <yang.husurianto@gmail.com>
2023-07-12 18:26:00 -0700

    Allow role/user permitting

--- commands/permit.js
+++ commands/permit.js
@@ -13,6 +13,28 @@ module.exports = function(repDB) {
 			if (!interaction.memberPermissions.has(PermissionsBitField.Flags.ADMINISTRATOR)) {
 				await interaction.reply("You do not have permission to run this command!");
 			}
+
+			const mention = interaction.options.getMentionable('target');
+			if (mention.hasOwnProperty('id')) { // check if mention was a role
+				let allowedRoles = await repDB.get("allowedRoles");
+
+				// if allowedRoles doesnt exist
+				if (!allowedRoles) allowedRoles = [];
+
+				allowedRoles.push(mention.id);
+				await repDB.set("allowedRoles", allowedRoles);
+				await interaction.reply(`The <@&${mention.id}> role has been added to the permit list.`);
+			} else { // else if mention was an user
+				let allowedUsers = await repDB.get("allowedUsers");
+
+				// if allowedUsers doesnt exist
+				if (!allowedUsers) allowedUsers = [];
+
+				allowedUsers.push(mention.user.id);
+				await repDB.set("allowedUsers", allowedUsers);
+				await interaction.reply(`<@${mention.user.id}> has been added to the permit list.`);
+			}
+
 		},
 	};
 };	
